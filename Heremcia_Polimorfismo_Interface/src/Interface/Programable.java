6757a47c6db26c6a3cacf18055c64fb8b0ae60de
astunlo <lopezgastonm@gmail.com>
2016-05-04 20:32:33 -0300

    Delete Programable.java

--- Heremcia_Polimorfismo_Interface/src/Interface/Programable.java
+++
@@ -1,9 +0,0 @@
-package Interface;
-
-import java.util.Scanner;
-
-public interface Programable {
-	static Scanner entrada=new Scanner(System.in);
-    
-    void cargarRequerimientosDeSistema();
-}
